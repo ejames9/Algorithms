e0307f0e9df8bf66831725baa5116613eed15542
ejames9 <foster9@icloud.com>
2017-12-14 20:33:28 -0800

    Added CS Quotes

--- JSON/csQuotes.js
+++ JSON/csQuotes.js
@@ -28,3 +28,28 @@ var steveJobsQuotes = [
   {'id': 19, 'quote': ["Taking LSD was a profound experience, one of the most important things in my life. LSD shows you that there’s﻿ another side to the coin... ", "and you can’t remember it when it wears off, but you know it. It reinforced my sense of what was important—creating great things... ", "instead of making money, putting things back into the stream of history and of human consciousness as much as I could."]},
   {'id': 20, 'quote': ["When you grow up you tend to get told that the world is the way it is and your life is just to live your life inside the world...", "Try not to bash into the walls too much. Try to have a nice family life, have fun, save a little money...", "That's a very limited life. Life can be much broader once you discover one simple fact: Everything around you that you call life was made up by people that were no smarter than you...", "And you can change it, you can influence it… Once you learn that, you'll never be the same again."]}
 ];
+
+
+var csQuotes = [
+  {'id': 0, 'author': "Edsger Dijkstra", 'quote': "Computer science is no more about computers than astronomy is about telescopes."},
+  {'id': 1, 'author': "Sebastian Thrun", 'quote': "To me, mathematics, computer science, and the arts are insanely related. They're all creative expressions."},
+  {'id': 2, 'author': "Donald Knuth", 'quote': "People think that computer science is the art of geniuses but the actual reality is the opposite, just many people doing things that build on eachother, like a wall of mini stones."},
+  {'id': 3, 'author': "Brendan Iribe", 'quote': "I like to think of it as this new field. Instead of computer science, it's going to be virtual science."},
+  {'id': 4, 'author': "Sebastian Thrun", 'quote': "You could get an entire computer science education for free right now."},
+  {'id': 5, 'author': "Alan Perlis", 'quote': "Computer Science is embarrassed by the computer."},
+  {'id': 6, 'author': "Jawed Karim", 'quote': "That's one of those things about being a computer science major: Valentine's Day is just another day."},
+  {'id': 7, 'author': "Jaron Lanier", 'quote': "We're losing track of the vastness of the potential for computer science. We really have to revive the beautiful intellectual joy of it, as opposed to the business potential."},
+  {'id': 8, 'author': "Larry Wall", 'quote': "I think computer science, by and large, is still stuck in the Modern age."},
+  {'id': 9, 'author': "will.i.am", 'quote': "It should be mandatory that you understand computer science."},
+  {'id': 10, 'author': "Edsger Dijkstra", 'quote': "The question of whether a computer can think is no more interesting than the question of whether a submarine can swim."},
+  {'id': 11, 'author': "Steve Jobs", 'quote': "Why join the navy if you can be a pirate?"},
+  {'id': 12, 'author': "Alan Kay", 'quote': "I don't know how many of you have ever met Dijkstra, but you probably know that arrogance in computer science is measured in nano-Dijkstras."},
+  {'id': 13, 'author': "C.A.R. Hoare", 'quote': "The most important property of a program is whether it accomplishes the intention of its user."},
+  {'id': 14, 'author': "Nick Black", 'quote': "...if you aren't, at any given time, scandalized by code you wrote five or even three years ago, you're not learning anywhere near enough"},
+  {'id': 15, 'author': "Dennis M. Ritchie", 'quote': "C is quirky, flawed, and an enormous success."},
+  {'id': 16, 'author': "Alan Perlis", 'quote': "Is it possible that software is not like anything else, that it is meant to be discarded: that the whole point is to always see it as a soap bubble?"},
+  {'id': 17, 'author': "Charles Petzold", 'quote': "Programming in machine code is like eating with a toothpick."},
+  {'id': 18, 'author': "William Barrett", 'quote': "The computer only gives back ourselves. It is a faithful mirror that reflects the human traits that are brought to it."},
+  {'id': 19, 'author': "Steve Jobs", 'quote': "Details matter, it's worth waiting to get it right."},
+  {'id': 20, 'author': "Donald Knuth", 'quote': "Overstimulation has been the real drawback. I need to find ways to stop thinking about analysis of algorithms, in order to do various other things that human beings ought to do."}
+];
