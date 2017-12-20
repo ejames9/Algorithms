ffa4eb5439c48a0ecb05c7a5dae17edc8c8c1d15
ejames9 <foster9@icloud.com>
2017-12-19 20:47:35 -0800

    Added weather data.

---
+++ JSON/weather.js
@@ -0,0 +1,53 @@
+const weather = {
+  "coord": {
+    "lon": -122.58,
+    "lat": 45.48
+  },
+  "weather": [
+    {
+      "id": 741,
+      "main": "Fog",
+      "description": "fog"
+    },
+    {
+      "id": 500,
+      "main": "Rain",
+      "description": "light rain",
+      "icon": "10n"
+    },
+    {
+      "id": 701,
+      "main": "Mist",
+      "description": "mist",
+      "icon": "50n"
+    }
+  ],
+  "base": "stations",
+  "main": {
+    "temp": 10.51,
+    "pressure": 1016,
+    "humidity": 100,
+    "temp_min": 8,
+    "temp_max": 12
+  },
+  "visibility": 9656,
+  "wind": {
+    "speed": 3.36,
+    "deg": 201.002
+  },
+  "clouds": {
+    "all": 90
+  },
+  "dt": 1513668240,
+  "sys": {
+    "type": 1,
+    "id": 2315,
+    "message": 0.1659,
+    "country": "US",
+    "sunrise": 1513698394,
+    "sunset": 1513729745
+  },
+  "id": 5736378,
+  "name": "Lents",
+  "cod": 200
+}
