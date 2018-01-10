e170b1c156df93f9cd010ea84ed766752f7eb781
ejames9 <foster9@icloud.com>
2018-01-10 10:34:42 -0800

    Add data processing file to be served via http for codepen app

--- JSON/dataProcessor.js
+++ JSON/dataProcessor.js
@@ -1,1 +1,21 @@
-/**/
+/*
+dataProcessor.js
+
+Code used to process data recieved from
+twitch.tv api calls.
+
+Eric James Foster, MIT license...
+*/
+
+
+
+// a Higher-Order function for piping smaller functions together
+// that operate on the same piece of data...
+const compose = (...funcs)=>
+  (arg)=>
+    funcs.reduce(
+      (data, func)=> func(data),
+      arg
+    )
+
+log('hello there ye');
